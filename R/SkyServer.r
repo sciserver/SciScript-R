75761d4efadfcf3d6dfb83b6901b0a8aed9217ff
glemson <gerard.lemson@gmail.com>
2016-06-24 07:23:21 -0400

    sdsd

--- R/SkyServer.r
+++ R/SkyServer.r
@@ -2,12 +2,11 @@ require(httr)
 require(jsonlite)
 require(utils)
 
-#' @export
+
 runSqlSearch  <- function(sql,limit="10", token=NULL)
 {
-  dataRelease = "DR12"
-  
-  url=paste("http://scitest02.pha.jhu.edu/SkyserverWS/", dataRelease, "/SearchTools/SqlSearch?",sep="")
+
+  url=paste(SkyServerWSurl, '/', DataRelease, "/SearchTools/SqlSearch?", sep="")
   url = paste(url,"format=csv&",sep="")
   url = paste(url,"cmd=",sql,"&",sep="")
   if(!is.null(limit)) {
