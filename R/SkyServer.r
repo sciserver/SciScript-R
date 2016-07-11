459ba1afde9f3b54d6f171c50f81a43f546d71cd
mtaghiza <mtaghiza@jhu.edu>
2016-07-11 15:14:47 -0400

    -Added more SkyServer functions.

--- R/SkyServer.r
+++ R/SkyServer.r
@@ -23,7 +23,7 @@ SkyServer.sqlSearch  <- function(sql,limit="10", token=NULL)
     writeLines(paste("Error: ",content(r)$'ErrorMessage',"\n","LogMessageID: ", content(r)$'LogMessageID',"\n","LogTime: ",content(r)$'LogTime',sep=""))
     return (NULL)
   } else {
-    t=read.csv(textConnection(content(r, encoding="UTF-8")))
+    t=read.csv(textConnection(content(r, encoding="UTF-8")), comment.char = "#")
     return(t)
   }
 
@@ -53,5 +53,64 @@ SkyServer.getJpegImgCutout  <- function(ra, dec, scale=0.7, width=512, height=51
   } else {
     sdssImage = content(r)
     return(sdssImage)
+  }
+}
+
+SkyServer.radialSearch  <- function(ra, dec, radius=1, coordType="equatorial", whichPhotometry="optical", limit="10", token=NULL)
+{
+  
+  url=paste(Config.SkyServerWSurl, '/', Config.DataRelease, "/SearchTools/RadialSearch?", sep="")
+  url = paste(url,"format=csv&",sep="")
+  url = paste(url,"ra=",ra,"&",sep="")
+  url = paste(url,"dec=",dec,"&",sep="")
+  url = paste(url,"radius=",radius,"&",sep="")
+  url = paste(url,"coordtype=",coordType,"&",sep="")
+  url = paste(url,"whichphotometry=",whichPhotometry,"&",sep="")
+  if(!is.null(limit)) {
+    url = paste(url,"limit=",limit,"&",sep="")
+  }
+  url = URLencode(url)
+  
+  if(is.null(token)) {
+    r= GET(url,accept("text/plain"))
+  } else {
+    r= GET(url,accept("text/plain"),add_headers('X-Auth-Token'=token))
+  }
+  if(r$status_code != 200) {
+    writeLines(paste("Error: ",content(r)$'ErrorMessage',"\n","LogMessageID: ", content(r)$'LogMessageID',"\n","LogTime: ",content(r)$'LogTime',sep=""))
+    return (NULL)
+  } else {
+    t=read.csv(textConnection(content(r, encoding="UTF-8")), comment.char = "#")
+    return(t)
+  }
+}
+
+SkyServer.rectangularSearch  <- function(min_ra, max_ra, min_dec, max_dec, coordType="equatorial", whichPhotometry="optical", limit="10", token=NULL)
+{
+  
+  url=paste(Config.SkyServerWSurl, '/', Config.DataRelease, "/SearchTools/RectangularSearch?", sep="")
+  url = paste(url,"format=csv&",sep="")
+  url = paste(url,"min_ra=",min_ra,"&",sep="")
+  url = paste(url,"max_ra=",max_ra,"&",sep="")
+  url = paste(url,"min_dec=",min_dec,"&",sep="")
+  url = paste(url,"max_dec=",max_dec,"&",sep="")
+  url = paste(url,"coordtype=",coordType,"&",sep="")
+  url = paste(url,"whichphotometry=",whichPhotometry,"&",sep="")
+  if(!is.null(limit)) {
+    url = paste(url,"limit=",limit,"&",sep="")
+  }
+  url = URLencode(url)
+  
+  if(is.null(token)) {
+    r= GET(url,accept("text/plain"))
+  } else {
+    r= GET(url,accept("text/plain"),add_headers('X-Auth-Token'=token))
+  }
+  if(r$status_code != 200) {
+    writeLines(paste("Error: ",content(r)$'ErrorMessage',"\n","LogMessageID: ", content(r)$'LogMessageID',"\n","LogTime: ",content(r)$'LogTime',sep=""))
+    return (NULL)
+  } else {
+    t=read.csv(textConnection(content(r, encoding="UTF-8")), comment.char = "#")
+    return(t)
   }
 }
