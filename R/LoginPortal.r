f9045a13c4b3c2faf58106869c56dd6cf352b10b
mtaghiza <mtaghiza@jhu.edu>
2017-02-17 09:34:01 -0500

    Updated deprecation of LoginPortal, and NAMESPACE definition

--- R/LoginPortal.r
+++ R/LoginPortal.r
@@ -2,26 +2,27 @@
 #require(jsonlite)
 
 #source('Config.r')
+#source('Authentication.r')
 
 # Deprecated. Use Authentication.getKeystoneUserWithToken instead. Returns a dictionary with name/id for the user corresponding to the specified token
 LoginPortal.getKeystoneUserWithToken<-function(token){
-  .Deprecated("Authentication")
-  return Authentication.getKeystoneUserWithToken(token)
+  .Deprecated("Authentication.getKeystoneUserWithToken")
+  return (Authentication.getKeystoneUserWithToken(token))
 }
 
 # Deprecated. Use Authentication.login instead. Login and return token which is then also set as the Sys environment variabl "sciservertoken"
 LoginPortal.login<-function(UserName, Password){
-  .Deprecated("Authentication")
-  return Authentication.login(UserName, Password){
+  .Deprecated("Authentication.login")
+  return (Authentication.login(UserName, Password))
 }
 
 #  Deprecated. Use Authentication.getKeystoneUserWithToken instead. 
 LoginPortal.getToken<-function(){
-  .Deprecated("Authentication")
+  .Deprecated("Authentication.getToken")
   Authentication.getToken()
 }
 # Deprecated. Use Authentication.setToken instead. 
 LoginPortal.setToken<-function(token){
-  .Deprecated("Authentication")
+  .Deprecated("Authentication.setToken")
   Authentication.setToken()
 }
