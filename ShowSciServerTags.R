78eb1b8da2784896f482f739d1d622c1cd6849de
mtaghiza <mtaghiza@jhu.edu>
2017-03-08 16:30:09 -0500

    Fix ShowSciserver Tags

--- ShowSciServerTags.R
+++ ShowSciServerTags.R
@@ -4,13 +4,12 @@ cat("\n---1) Updating local Git repository...\n\n")
 system("git tag -d $(git tag)") #deletes local tags
 system("git fetch --all") #fetches all remotes into local repo, including tags.
 system("git checkout master")
-system("git reset --hard origin/master") #resets the local master branch to what was just fetched.
-system("git clean -df") #removes all untracked files
 
-cat("\n--2) Listing available SciServer version Tags:\n\n")
-tags = system("git tag", intern=TRUE)
+cat("\n--2) Listing available SciServer version tags:\n\n")
+tags = system('git tag --list "*sciserver*"', intern=TRUE)
 if(length(tags)==0){
   cat("No SciServer Tags available.\n\n")
 }else{
-  system("git tag")
+  system('git tag --list "*sciserver*"')
 }
+cat("\n*** Refer to http://www.sciserver.org/support/updates for particular release tag details.")
