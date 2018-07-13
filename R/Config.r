1d657735be2ffdd0988c800a43dacfb1035fee55
Joseph Jon Booker <joe@neoturbine.net>
2018-07-13 11:01:28 -0400

    Load configs from /etc/sciserver/sciscript.json if it exists
    This file overrides any values built into Config.r, but is overridable
    by ~/.config/sciserver/sciscript.json (if it exists)

--- R/Config.r
+++ R/Config.r
@@ -50,6 +50,27 @@ Config.isSciServerComputeEnvironment<-function()
   }
 }
 
+.onLoad <- function(libname, pkgname) {
+	readSciServerConfig <- function(filename) {
+		if (file.exists(filename)) {
+			config.file.data <- jsonlite::fromJSON(SCISERVER_CONFIG)
+			Config.CasJobsRESTUri <<- if (is.null(config.file.data$CasJobsRESTUri)) SciServer:::Config.CasJobsRESTUri else config.file.data$CasJobsRESTUri
+			Config.AuthenticationURL <<- if (is.null(config.file.data$AuthenticationURL)) SciServer:::Config.AuthenticationURL else config.file.data$AuthenticationURL
+			Config.SciDriveHost <<- if (is.null(config.file.data$SciDriveHost)) SciServer:::Config.SciDriveHost else config.file.data$SciDriveHost
+			Config.SkyQueryUrl <<- if (is.null(config.file.data$SkyQueryUrl)) SciServer:::Config.SkyQueryUrl else config.file.data$SkyQueryUrl
+			Config.SkyServerWSurl <<- if (is.null(config.file.data$SkyServerWSurl)) SciServer:::Config.SkyServerWSurl else config.file.data$SkyServerWSurl
+			Config.DataRelease <<- if (is.null(config.file.data$DataRelease)) SciServer:::Config.DataRelease else config.file.data$DataRelease
+			Config.KeystoneTokenFilePath <<- if (is.null(config.file.data$KeystoneTokenPath)) SciServer:::Config.KeystoneTokenFilePath else config.file.data$KeystoneTokenPath
+			Config.Version <<- if (is.null(config.file.data$version)) SciServer:::Config.Version else config.file.data$version
+		}
+	}
+
+	SYSTEM_CONFIG_DIR <- '/etc'
+	CONFIG_DIR <- Sys.getenv("XDG_CONFIG_HOME", file.path(normalizePath("~"), ".config"))
+
+	for (configDir in c(SYSTEM_CONFIG_DIR, CONFIG_DIR)) {
+		readSciServerConfig(file.path(configDir, 'sciserver', 'sciscript.json'))
+}
 
 # returns TRUE if the library is running inside the SciServer-Compute, and FALSE if not
 Config.isSciServerComputeEnvironment<-function()
